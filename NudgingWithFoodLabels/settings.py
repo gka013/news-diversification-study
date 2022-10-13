192def0a552f10a0041481056771e8f56a2d8172
Jeng <positive66progress@gmail.com>
2022-10-13 11:52:20 +0200

    tweaked requirements version

--- NudgingWithFoodLabels/settings.py
+++ NudgingWithFoodLabels/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = 'gasdfasdgae-supersecret-donotsharethis-important-!!!!#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 #ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0', 'localhost','newsresearchuib.herokuapp.com', 'newsresearch.mediafutures.no', '158.37.65.236'] # 158.39.201.22 #127.0.0.1', 'localhost'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','newsresearchuib.herokuapp.com']
 #CSRF_TRUSTED_ORIGINS = ['https://newsresearch.mediafutures.no']
-#CSRF_TRUSTED_ORIGINS = ['newsresearchuib.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://newsresearchuib.herokuapp.com']
 
 # Application definition
 
